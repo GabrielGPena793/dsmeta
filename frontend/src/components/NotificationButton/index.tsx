76bc4a9034b5fc5536cc872d37828122afff8620
gabrielgpena793 <kbeca.gabriel793@gmail.com>
2022-07-16 03:11:52 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,22 @@
+import axios from "axios";
 import notificationImg from "../../assets/img/notificaiton-icon.svg";
+import { BASE_URL } from "../../utils/request";
 import "./styles.css";
 
-export function NotificationButton() {
+interface NotificationButtonProps {
+  saleId: number;
+}
+
+function handleSendNotification(saleId : number){
+  axios.get(`${BASE_URL}/sales/${saleId}/notification`)
+}
+
+
+export function NotificationButton({ saleId } : NotificationButtonProps) {
+
+
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleSendNotification(saleId)}>
       <img src={notificationImg} alt="Notificar" />
     </div>
   )
